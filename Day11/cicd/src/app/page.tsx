2e078ee36b45434dc66521ef866b649ed8944046
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-09-18 11:35:24 +0900

    husky error directory test

--- Day11/cicd/src/app/page.tsx
+++ Day11/cicd/src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import sum from "./utils/sum"
 
 export default function Home() {
+  const a = 1;
+  const b = 2;
   return (
     <div>
+      `${a} + ${b} = ${sum(a, b)}`
     </div>
   );
 }
